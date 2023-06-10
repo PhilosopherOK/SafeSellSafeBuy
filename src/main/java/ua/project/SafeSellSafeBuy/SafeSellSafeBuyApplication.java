62ec600958186c6cabc82f09af6460f73a3b859a
Hnatiuk Rostyslav <hnatiukrost@gmail.com>
2023-06-10 07:52:15 +0300

    add stripe payments, neew correct

--- src/main/java/ua/project/SafeSellSafeBuy/SafeSellSafeBuyApplication.java
+++ src/main/java/ua/project/SafeSellSafeBuy/SafeSellSafeBuyApplication.java
@@ -1,15 +1,22 @@
 package ua.project.SafeSellSafeBuy;
 
+import com.stripe.Stripe;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-
+import javax.annotation.PostConstruct;
 
 
 @SpringBootApplication
 public class SafeSellSafeBuyApplication {
+
+    @PostConstruct
+    public void setup(){
+        Stripe.apiKey = "sk_test_51MzlRYFqvRFvrqeTIGUTEjV3Z0LhMNF1ujwBW8GGKVMWirqKHrNzAGmwUY4FW3ee2QGZT3jdMX4Zua99rampRmrg005lMI2PeZ";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SafeSellSafeBuyApplication.class, args);
     }
---
+++ src/main/java/ua/project/SafeSellSafeBuy/dto/CreatePayment.java
@@ -0,0 +1,12 @@
+package ua.project.SafeSellSafeBuy.dto;
+
+import com.google.gson.annotations.SerializedName;
+
+public class CreatePayment {
+    @SerializedName("items")
+    Object[]items;
+
+    public Object[] getItems() {
+        return items;
+    }
+}
